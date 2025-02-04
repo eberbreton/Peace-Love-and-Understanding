d14dde211d70afa4b1c86dd28b4ec7e227c1f3e7
eberbreton <eberbreton200@gmail.com>
2025-02-04 11:15:00 -0800

    Initial Commit

---
+++ Peace, Love, and Understanding/Peace__Love__and_UnderstandingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Peace__Love__and_UnderstandingApp.swift
+//  Peace, Love, and Understanding
+//
+//  Created by Student1 on 2/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Peace__Love__and_UnderstandingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
